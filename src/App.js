75238ccb831bfb3798c7d0a7e8db934473e4b5a6
Md. Mehedi Hasan <mehedi53423@gmail.com>
2022-10-28 13:02:22 +0600

    Tailwind CSS Added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="">
+      <h1 className="border">Programming Mastery</h1>
+    </div>
+  );
 }
 
 export default App;
