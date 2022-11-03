f29c6c8c7a3ff1c7ee83d6c24ef6bfabf43ee525
Md. Mehedi Hasan <mehedi53423@gmail.com>
2022-11-03 12:36:41 +0600

    Footer Added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,8 +2,27 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer>
-      <h1>&#169; Programming Mastery</h1>
+    <footer className="container mx-auto border-t-2 mt-5 pt-5">
+      <div className="flex justify-between font-bold text-lg">
+        <h1>&#169; 2022 Programming Mastery&#8482;. All Rights Reserved.</h1>
+        <div className="">
+          <a href="https://www.facebook.com">
+            <i className="fa-brands fa-facebook fa-xl px-5"></i>
+          </a>
+          <a href="https://www.twitter.com">
+            <i className="fa-brands fa-twitter fa-xl px-5"></i>
+          </a>
+          <a href="https://www.github.com">
+            <i className="fa-brands fa-github fa-xl px-5"></i>
+          </a>
+          <a href="https://www.gitlab.com">
+            <i className="fa-brands fa-gitlab fa-xl px-5"></i>
+          </a>
+          <a href="https://www.bitbucket.com">
+            <i className="fa-brands fa-bitbucket fa-xl px-5"></i>
+          </a>
+        </div>
+      </div>
     </footer>
   );
 };
