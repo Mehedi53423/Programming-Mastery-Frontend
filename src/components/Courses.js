dc3ea69bb1ba08b838939668ae7f8a1802a9561c
Md. Mehedi Hasan <mehedi53423@gmail.com>
2022-11-24 14:48:33 +0600

    Mobile Responsive Courses Page Added

--- src/components/Courses.js
+++ src/components/Courses.js
@@ -7,13 +7,13 @@ const Courses = () => {
   const courses = useLoaderData();
   return (
     <div className="container mx-auto md:mt-36 mt-10 md:mb-24">
-      <div className="flex">
-        <div className="w-1/6 fixed">
+      <div className="md:flex">
+        <div className="md:w-1/6 md:fixed hidden md:block">
           <Sidebar className=""></Sidebar>
         </div>
-        <div className="w-3/12"></div>
-        <div className="w-5/6 mt-2">
-          <div className="grid grid-cols-3 gap-6">
+        <div className="md:w-3/12"></div>
+        <div className="md:w-5/6 mt-2">
+          <div className="grid md:grid-cols-3 grid-cols-1 gap-6">
             {courses.map((course) => (
               <CourseCard key={course.id} course={course}></CourseCard>
             ))}
